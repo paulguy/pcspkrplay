9a1b78c51e12a96f361744621302e11126cac50e
Paul LaMendola <paulguy119@gmail.com>
2011-09-17 21:58:19 -0400

    Added command line parsing runtime enabling of decompile mode no playback mode ability to enable/disable status display built-in playback from file Coded in default playback device, with ability to select a different one

--- pcspkrplay.c
+++ pcspkrplay.c
@@ -13,23 +13,116 @@
 #include "output.h"
 #include "str.h"
 
+char DEFAULT_DEVICE[] =  "/dev/input/by-path/platform-pcspkr-event-spkr";
+
 void statusout(int cur, int max) {
 	fprintf(stderr, "%u/%u\r", cur, max);
 }
 
+void nostatus(int cur, int max) {
+	return;
+}
+
+void usage(char *name) {
+	fprintf(stderr, "USAGE: %s <options>\n\
+Options:\n\
+--decompile / -d          - Decompile string, used for outputting size-optimized code.\n\
+--device    / -e device   - Select event device, default is %s.\n\
+--filename  / -f filename - Read from filename instead of standard input.\n\
+--quiet     / -q          - Don't play, only compile (and optionally decompile) then quit.\n\
+--display   / -s          - Display playback status.\n\
+--help      / -h          - Print help/usage.  Refer to README for more information.\n\
+", name, DEFAULT_DEVICE);
+	exit(-1);
+}
+
 int main(int argc, char **argv) {
+	int i;
+
 	int speaker;
+	int sndcap;
+
 	song *s;
 	str *sstr;
 
-	int sndcap;
+	char arg;
+	char *device;
+	FILE *in;
+	int decompile, playback, display;
 
-	if(argc < 2) {
-		fprintf(stderr, "USAGE: %s <path to event device>\n", argv[0]);
-		exit(-1);
+	device = DEFAULT_DEVICE;
+	in = stdin;
+	decompile = 0;
+	playback = 1;
+	display = 0;
+
+	for(i = 1; i < argc; i++) {
+		if(argv[i][0] == '-') {
+			if(strlen(argv[i]) < 2)
+				usage(argv[0]);
+			if(argv[i][1] == '-') {
+				if(strlen(argv[i]) < 3)
+					usage(argv[0]);
+				if(strcmp(&(argv[i][2]), "decompile") == 0)
+					arg = 'd';
+				else if(strcmp(&(argv[i][2]), "device") == 0)
+					arg = 'e';
+				else if(strcmp(&(argv[i][2]), "filename") == 0)
+					arg = 'f';
+				else if(strcmp(&(argv[i][2]), "help") == 0)
+					arg = 'h';
+				else if(strcmp(&(argv[i][2]), "quiet") == 0)
+					arg = 'q';
+				else if(strcmp(&(argv[i][2]), "display") == 0)
+					arg = 'd';
+				else
+					usage(argv[0]);
+			} else {
+				arg = argv[i][1];
+			}
+			switch(arg) {
+				case 'd':
+					decompile = 1;
+					break;
+				case 'e':
+					if(i + 1 < argc) {
+						device = argv[i + 1];
+						i++;
+					} else {
+						usage(argv[0]);
+					}
+					break;
+				case 'f':
+					if(i + 1 < argc) {
+						if(in != stdin)
+							fclose(in);
+						in = fopen(argv[i + 1], "rb");
+						if(in == NULL) {
+							fprintf(stderr, "Couldn't open file %s.\n", argv[i + 1]);
+							exit(-1);
+						}
+						i++;
+					} else {
+						usage(argv[0]);
+					}
+					break;
+				case 'q':
+					playback = 0;
+					break;
+				case 's':
+					display = 1;
+					break;
+				case 'h':
+				default:
+					usage(argv[0]);
+					break;
+			}
+		} else {
+			usage(argv[0]);
+		}
 	}
 
-	speaker = open(argv[1], O_RDWR);
+	speaker = open(device, O_RDWR);
 	if(speaker == -1) {
 		perror("open");
 		exit(-2);
@@ -49,21 +142,27 @@ int main(int argc, char **argv) {
 	if(sstr == NULL)
 		return(-1);
 
-	sstr = readstream(stdin);
+	sstr = readstream(in);
 	s = compilesong(sstr->data, sstr->strsize);
 	if(s == NULL) {
 		fprintf(stderr, "Song compile failed.\n");
 		exit(-5);
 	}
 	freestr(sstr);
-#ifdef DECOMPILER
-	rewindsong(s);
-	sstr = decompilesong(s);
-	fprintf(stderr, "%s\n", sstr->data);
-#endif
-	rewindsong(s);
-	playsong(speaker, s, statusout);
-	fprintf(stderr, "\n");
+	if(decompile == 1) {
+		rewindsong(s);
+		sstr = decompilesong(s);
+		fprintf(stderr, "%s\n", sstr->data);
+	}
+	if(playback == 1) {
+		rewindsong(s);
+		if(display == 1) {
+			playsong(speaker, s, statusout);
+			fprintf(stderr, "\n");
+		} else {
+			playsong(speaker, s, nostatus);
+		}
+	}
 
 	exit(0);
 }
